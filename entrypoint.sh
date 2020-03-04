f7e795cd24bd1ebf6224d5312b9abf143a04a202
Nicolas Vargas <nvortega92@gmail.com>
2020-03-04 22:23:42 +0100

    Disabling strict host key checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,4 +17,4 @@ chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
 chmod 600 "$SSHPATH/server_key"
 
-ssh $INPUT_ARGS -i $SSHPATH/server_key -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
+ssh $INPUT_ARGS -i $SSHPATH/server_key -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
