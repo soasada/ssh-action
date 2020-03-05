2a71df1b72d528db5a2ad77821b32bb72403c280
Nicolas Vargas <nvortega92@gmail.com>
2020-03-05 22:43:25 +0100

    Changed permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ touch "$SSHPATH/known_hosts"
 echo "$INPUT_KEY" > $SSHPATH/id_rsa
 
 chmod 700 $SSHPATH
-chmod 400 $SSHPATH/known_hosts
-chmod 400 $SSHPATH/id_rsa
+chmod 600 $SSHPATH/known_hosts
+chmod 600 $SSHPATH/id_rsa
 
 ssh $INPUT_ARGS -i $SSHPATH/id_rsa -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
