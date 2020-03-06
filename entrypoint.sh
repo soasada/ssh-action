205679178ccbde94f1f091d3084e9373d3ff6d81
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 20:50:17 +0100

    Cat the known_hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,9 @@ mkdir -p ~/.ssh
 
 echo "$INPUT_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
-chmod 600 ~/.ssh/known_hosts
+#echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
+#chmod 600 ~/.ssh/known_hosts
 
+ssh $INPUT_ARGS -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
 
-ssh $INPUT_ARGS -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
+cat ~/.ssh/known_hosts
