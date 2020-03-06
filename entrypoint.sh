bf450656ae822b9f923f0e26f424436d33728535
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 21:35:02 +0100

    Change permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,5 +23,6 @@ echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
 chmod 600 ~/.ssh/known_hosts
 echo "UserKnownHostsFile ~/.ssh/known_hosts" >> ~/.ssh/ssh_config
 chmod 600 ~/.ssh/ssh_config
+chmod 666 /dev/tty
 
 ssh $INPUT_ARGS -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
