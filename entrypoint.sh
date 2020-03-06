0a13ea262dc3b95b2dabfaf42f0c9f6129ddcc6c
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 20:52:26 +0100

    Added known host file

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,8 @@ chmod 600 ~/.ssh/id_rsa
 #echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
 #chmod 600 ~/.ssh/known_hosts
 
+touch ~/.ssh/known_hosts
+
 ssh $INPUT_ARGS -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
 
 cat ~/.ssh/known_hosts
