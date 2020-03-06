7882a1d849fcf3ffcf727444734327e3af42b70a
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 21:01:57 +0100

    Show pub key

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,17 @@ INPUT_HOST=$1
 INPUT_USERNAME=$2
 INPUT_PORT=$3
 INPUT_KEY=$4
-INPUT_PUBKEY=$5
-INPUT_SCRIPT=$6
-INPUT_ARGS=$7
+INPUT_CLIENTPUBKEY=$5
+INPUT_PUBKEY=$6
+INPUT_SCRIPT=$7
+INPUT_ARGS=$8
 
 mkdir -p ~/.ssh
 
 echo "$INPUT_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+#echo "$INPUT_CLIENTPUBKEY" > ~/.ssh/id_rsa.pub
+#chmod 600 ~/.ssh/id_rsa.pub
 #echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
 #chmod 600 ~/.ssh/known_hosts
 echo "UserKnownHostsFile ~/.ssh/known_hosts" > /etc/ssh/ssh_config
@@ -22,3 +25,4 @@ touch ~/.ssh/known_hosts
 ssh $INPUT_ARGS -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
 
 cat ~/.ssh/known_hosts
+ssh-keygen -H -F $INPUT_HOST
