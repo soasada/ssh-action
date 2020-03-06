7a3689f7dc6f13f4c20b9d4536ccdd9768193103
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 21:20:25 +0100

    Testing with server pub key

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,9 @@ echo "$INPUT_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo "$INPUT_CLIENTPUBKEY" > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa.pub
-#echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
-#chmod 600 ~/.ssh/known_hosts
+echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
 echo "UserKnownHostsFile ~/.ssh/known_hosts" >> ~/.ssh/ssh_config
 chmod 600 ~/.ssh/ssh_config
-echo "|1|DVgp+hFNLoNcge9ugS3bKBFg23k=|pq7ITTZP8D0MqoVFvNQPq9lYgc8= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPiABjYZaLieP2ZxpUeZb0HfXVpjNW83M3cNVO6gDrpV6Pkgh7OwL6Ixt3ZOsDjql47yz6XSoaC94BNTqBrzwvw=" >> ~/.ssh/known_hosts
-chmod 600 ~/.ssh/known_hosts
 
 ssh $INPUT_ARGS -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
