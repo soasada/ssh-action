4599ef5e6f96590f03ae88756c566798bd3be276
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 21:18:17 +0100

    Testing with client public key

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,16 @@ INPUT_USERNAME=$2
 INPUT_PORT=$3
 INPUT_KEY=$4
 INPUT_PUBKEY=$5
-INPUT_SCRIPT=$6
-INPUT_ARGS=$7
+INPUT_CLIENTPUBKEY=$6
+INPUT_SCRIPT=$7
+INPUT_ARGS=$8
 
 mkdir -p ~/.ssh
 
 echo "$INPUT_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-#echo "$INPUT_CLIENTPUBKEY" > ~/.ssh/id_rsa.pub
-#chmod 600 ~/.ssh/id_rsa.pub
+echo "$INPUT_CLIENTPUBKEY" > ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa.pub
 #echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
 #chmod 600 ~/.ssh/known_hosts
 echo "UserKnownHostsFile ~/.ssh/known_hosts" >> ~/.ssh/ssh_config
