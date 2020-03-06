21c041bb8acaadadb954104da36a9afaa74644ff
Nicolas Vargas <nvortega92@gmail.com>
2020-03-06 20:56:41 +0100

    Testing ssh_config

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ echo "$INPUT_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 #echo "$INPUT_PUBKEY" > ~/.ssh/known_hosts
 #chmod 600 ~/.ssh/known_hosts
-
+echo "UserKnownHostsFile ~/.ssh/known_hosts" > /etc/ssh/ssh_config
 touch ~/.ssh/known_hosts
 
 ssh $INPUT_ARGS -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -p $INPUT_PORT ${INPUT_USERNAME}@${INPUT_HOST} "$INPUT_SCRIPT"
